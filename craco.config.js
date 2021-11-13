895e719ef6eb7bb6715b34f788611e1858d37ff9
Alexander Mischuk <mischuk.alexander@gmail.com>
2021-11-13 16:50:42 +0200

    update webpack config

--- craco.config.js
+++ craco.config.js
@@ -10,26 +10,5 @@ module.exports = {
               tsConfigPath: "./tsconfig.paths.json"
            }
         }
-    ],
-    webpack: {
-        configure: (webpackConfig, {env, paths}) => {
-            if ( env === 'development' ) return { ...webpackConfig };
-            return {
-                ...webpackConfig,
-                entry: {
-                    main: [env === 'development' && require.resolve('react-dev-utils/webpackHotDevClient'),paths.appIndexJs].filter(Boolean),
-                    content: './src/chrome/content.ts',
-                    background: './src/chrome/background.ts'
-                },
-                output: {
-                    ...webpackConfig.output,
-                    filename: 'static/js/[name].js',
-                },
-                optimization: {
-                    ...webpackConfig.optimization,
-                    runtimeChunk: false,
-                },
-            }
-        },
-    }
+    ]
 }
